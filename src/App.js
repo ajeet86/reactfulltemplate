d8379687999e4899bace343fc5195b6dd7836f43
Ajeet Kumar <ajeetkumar@virtual.local>
2019-09-30 12:57:24 +0530

    first commite

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './assets/abc.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+	    <div className="App-logo">
+			<img src={logo} className="App-logo" alt="logo" />
+		</div>
+        
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         please check it 
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+		
+		</header>
+		
+		 <container className="App-container">
+			This is the container area
+		</container>
+		
+		<footer className="App-footer-container" > is the footer containt area 
+		</footer>
+     </div>
+    
   );
 }
 
